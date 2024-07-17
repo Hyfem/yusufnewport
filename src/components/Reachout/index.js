7d6771f4789a5eabb1dbca4857ffc89fff67a29a
Hyfem <144174535+Hyfem@users.noreply.github.com>
2024-07-17 08:19:01 -1000

    Update index.js

--- src/components/Reachout/index.js
+++ src/components/Reachout/index.js
@@ -1,1 +1,33 @@
+import './imdex.css'
 
+const Reachout = () => {
+    return(
+        <div className='co'>
+            <div className='left'>
+                <div className='content-container'>
+                    <form>
+                        <ul>
+                            <li className='half'>
+                                < input type='text' name='name' placeholder='Name' required />
+                                
+                            </li>
+                        </ul>
+
+
+                    </form>
+                </div>
+
+
+            </div>
+            <div className='right'>
+                <div className='content-container'>
+                    <p>a</p>
+                </div>
+
+            </div>
+
+
+        </div>
+    )
+}
+export default Reachout;
